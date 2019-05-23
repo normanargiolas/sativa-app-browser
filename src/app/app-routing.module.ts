751b8d363e6a7e18d553f2c2eb4a3a2cad9d37fc
NamroN <normanargiolas@gmail.com>
2019-05-23 21:14:37 +0200

    welcome component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {LoginComponent} from "./login/login.component";
+import {WelcomeComponent} from "./welcome/welcome.component";
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent}
+  {path: '', component: LoginComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'welcome', component: WelcomeComponent}
 ];
 
 @NgModule({
